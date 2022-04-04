c31e2811aaf1abce9d49b06847b00939de712ff5
N4-t <natanaelsouza2005@hotmail.com>
2022-04-04 10:03:08 -0300

    Impl de entrada de dados

--- src/RelogioDeAtleta.java
+++ src/RelogioDeAtleta.java
@@ -1,8 +1,34 @@
 
 public class RelogioDeAtleta {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
+	
+	
+	int cardiaRepouso = 60;	
+	int cardiaAtual = 119;
+	int oxigenio = 98;
+	int calculoUm = cardiaRepouso * 3;
+	int calculoDois = cardiaRepouso * 2;
+		
+		
+		 
+		 if(cardiaAtual > calculoUm || cardiaAtual < oxigenio){
+			 System.out.println("Diminuir");
+		 }
+		 
+		 
+		 else if (cardiaAtual < calculoDois && oxigenio > 97) {
+			 System.out.println("Aumentar");
+		 }
+		 
+		 else{
+			 System.out.println("manter");
+		 }
+		 
+		 
+		 
+		 
+		
 
 	}
 
